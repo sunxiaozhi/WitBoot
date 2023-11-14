d8a20560584f68b738e591efeac4085dbd97310a
sunxiaozhi <1129535445@qq.com>
2023-11-14 18:50:50 +0800

    RedisConfiguration

---
+++ witboot-infrastructure/src/main/java/com/witboot/infrastructure/config/RedisConfiguration.java
@@ -0,0 +1,13 @@
+package com.witboot.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Redis配置类
+ *
+ * @author sunxiaozhi
+ */
+@Configuration
+public class RedisConfiguration {
+
+}
