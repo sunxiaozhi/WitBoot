90fcc8abad725a2eabb23551d78aa558aa940de6
sunxiaozhi <1129535445@qq.com>
2023-11-15 20:05:11 +0800

    Redis序列化和反序列化配置

--- witboot-infrastructure/src/main/java/com/witboot/infrastructure/config/RedisConfiguration.java
+++ witboot-infrastructure/src/main/java/com/witboot/infrastructure/config/RedisConfiguration.java
@@ -1,6 +1,9 @@
 package com.witboot.infrastructure.config;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> e8d2a37 (Redis序列化和反序列化配置)
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
@@ -21,9 +24,12 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
+<<<<<<< HEAD
 =======
 import org.springframework.context.annotation.Configuration;
 >>>>>>> d1eebc6 (RedisConfiguration)
+=======
+>>>>>>> e8d2a37 (Redis序列化和反序列化配置)
 
 /**
  * Redis配置类
@@ -31,6 +37,7 @@ import org.springframework.context.annotation.Configuration;
  * @author sunxiaozhi
  */
 @Configuration
+<<<<<<< HEAD
 <<<<<<< HEAD
 @EnableCaching
 public class RedisConfiguration {
@@ -96,7 +103,73 @@ public class RedisConfiguration {
                 .build();
     }
 =======
+=======
+@EnableCaching
+>>>>>>> e8d2a37 (Redis序列化和反序列化配置)
 public class RedisConfiguration {
+    @Value("${spring.data.redis.time-to-live}")
+    private Duration timeToLive = Duration.ZERO;
 
+<<<<<<< HEAD
 >>>>>>> d1eebc6 (RedisConfiguration)
+=======
+    /**
+     * 配置Jackson2JsonRedisSerializer序列化策略
+     */
+    private Jackson2JsonRedisSerializer<Object> serializer() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+
+        // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会跑出异常
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.WRAPPER_ARRAY);
+
+        // 使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
+        return new Jackson2JsonRedisSerializer<>(objectMapper, Object.class);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        // 用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
+        redisTemplate.setValueSerializer(serializer());
+
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+        // 使用StringRedisSerializer来序列化和反序列化redis的key值
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+
+        // hash的key也采用String的序列化方式
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+
+        // hash的value序列化方式采用jackson
+        redisTemplate.setHashValueSerializer(serializer());
+
+        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory factory) {
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        // 配置序列化（解决乱码的问题）
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                // 缓存有效期
+                .entryTtl(timeToLive)
+                // 使用StringRedisSerializer来序列化和反序列化redis的key值
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
+                // 使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer()))
+                // 禁用空值
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(factory)
+                .cacheDefaults(config)
+                .build();
+    }
+>>>>>>> e8d2a37 (Redis序列化和反序列化配置)
 }
