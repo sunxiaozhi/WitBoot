d21b30d6b537a19fa9b8885a9cb1645f94d85816
sunxiaozhi <1129535445@qq.com>
2024-01-01 17:18:09 +0800

    恢复代码 由于修改提交作者问题引起

--- witboot-infrastructure/src/main/java/com/witboot/infrastructure/common/utils/RedisUtil.java
+++ witboot-infrastructure/src/main/java/com/witboot/infrastructure/common/utils/RedisUtil.java
@@ -1,66 +1,16 @@
 package com.witboot.infrastructure.common.utils;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8e37e7e (RedisUtil RedisUtilTest)
-=======
->>>>>>> 28ed750 (RedisUtil RedisUtilTest)
-=======
->>>>>>> b877f37 (日志等级)
-=======
-=======
->>>>>>> 8e37e7e (RedisUtil RedisUtilTest)
->>>>>>> 5414a73 (RedisUtil RedisUtilTest)
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 5414a73 (RedisUtil RedisUtilTest)
-=======
->>>>>>> ca56e17 (日志等级)
-=======
->>>>>>> 8e37e7e (RedisUtil RedisUtilTest)
-<<<<<<< HEAD
-=======
->>>>>>> 91070f1 (日志等级)
-=======
->>>>>>> 28ed750 (RedisUtil RedisUtilTest)
-=======
-=======
->>>>>>> ca56e17 (日志等级)
->>>>>>> b877f37 (日志等级)
-=======
->>>>>>> 5414a73 (RedisUtil RedisUtilTest)
 /**
  * Redis工具类
  *
  * @author sunxiaozhi
  */
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8e37e7e (RedisUtil RedisUtilTest)
-=======
->>>>>>> 28ed750 (RedisUtil RedisUtilTest)
-=======
->>>>>>> b877f37 (日志等级)
-=======
->>>>>>> 5414a73 (RedisUtil RedisUtilTest)
+
 @Component
 public class RedisUtil {
     @Autowired
@@ -96,61 +46,4 @@ public class RedisUtil {
     public boolean deleteCache(String key) {
         return redisTemplate.delete(key);
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-public class RedisUtil {
->>>>>>> ca56e17 (日志等级)
-=======
-<<<<<<< HEAD
->>>>>>> 8e37e7e (RedisUtil RedisUtilTest)
-=======
-public class RedisUtil {
->>>>>>> 91070f1 (日志等级)
-=======
->>>>>>> 28ed750 (RedisUtil RedisUtilTest)
-=======
-=======
-public class RedisUtil {
->>>>>>> ca56e17 (日志等级)
->>>>>>> b877f37 (日志等级)
-=======
-@Component
-public class RedisUtil {
-    @Autowired
-    public RedisTemplate redisTemplate;
-
-    /**
-     * 缓存数据
-     *
-     * @param key   键
-     * @param value 值
-     */
-    public <T> void setCache(String key, T value) {
-        redisTemplate.opsForValue().set(key, value);
-    }
-
-    /**
-     * 获取缓存
-     *
-     * @param key 键
-     * @return 值
-     */
-    public <T> T getCache(String key) {
-        ValueOperations<String, T> operations = redisTemplate.opsForValue();
-        return operations.get(key);
-    }
-
-    /**
-     * 删除缓存
-     *
-     * @param key 键
-     * @return 结果
-     */
-    public boolean deleteCache(String key) {
-        return redisTemplate.delete(key);
-    }
->>>>>>> 8e37e7e (RedisUtil RedisUtilTest)
->>>>>>> 5414a73 (RedisUtil RedisUtilTest)
 }
