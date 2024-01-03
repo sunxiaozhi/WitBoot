21f6b5ef8a240127c38004828ebca08d0019c01c
sunxiaozhi <1129535445@qq.com>
2024-01-03 21:28:17 +0800

    Jwt拦截器过滤

--- witboot-infrastructure/src/main/java/com/witboot/infrastructure/filter/JwtAuthenticationFilter.java
+++ witboot-infrastructure/src/main/java/com/witboot/infrastructure/filter/JwtAuthenticationFilter.java
@@ -21,6 +21,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+        filterChain.doFilter(request, response);
     }
 }
