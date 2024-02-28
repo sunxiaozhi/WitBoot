9d91ced5f2e8d8ffbd01e4e7ebaad73f69ae772f
sunxiaozhi <1129535445@qq.com>
2024-02-28 20:42:54 +0800

    删除无用配置

--- witboot-infrastructure/src/main/java/com/witboot/infrastructure/config/MybatisConfiguration.java
+++ witboot-infrastructure/src/main/java/com/witboot/infrastructure/config/MybatisConfiguration.java
@@ -1,12 +1,9 @@
 package com.witboot.infrastructure.config;
 
-import org.mybatis.spring.annotation.MapperScan;
-
 /**
  * Mybatis配置
  *
  * @author sunxiaozhi
  */
-@MapperScan("com.witboot.domain.mybatis.mapper")
 public class MybatisConfiguration {
 }
