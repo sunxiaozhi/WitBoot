5b923c4a409f3b93f50aaeeeb51dab369bf08a3d
sunxiaozhi <1129535445@qq.com>
2024-04-03 17:25:35 +0800

    引入devtools

--- witboot-ui/src/main.ts
+++ witboot-ui/src/main.ts
@@ -2,9 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { devtools } from '@vue/devtools'
 
 import App from './App.vue'
 import router from './router'
+
+if (process.env.NODE_ENV === 'development')
+  devtools.connect('http://localhost', 8098)
 
 const app = createApp(App)
 
