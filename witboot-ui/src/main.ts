b3d164215042ad2f50c86b28ab3c8b803754bdfa
sunxiaozhi <1129535445@qq.com>
2024-04-09 17:43:13 +0800

    初始引入element-plus

--- witboot-ui/src/main.ts
+++ witboot-ui/src/main.ts
@@ -7,6 +7,9 @@ import { devtools } from '@vue/devtools'
 import App from './App.vue'
 import router from './router'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 if (process.env.NODE_ENV === 'development')
   devtools.connect('http://localhost', 8098)
 
@@ -14,5 +17,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(ElementPlus)
 
 app.mount('#app')
