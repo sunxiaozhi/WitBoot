ba9272477ae452a380bcdb8c706067ac9ae987cf
sunxiaozhi <1129535445@qq.com>
2024-04-15 15:06:05 +0800

    全局引入图标

--- witboot-ui/src/main.ts
+++ witboot-ui/src/main.ts
@@ -9,6 +9,7 @@ import router from './router'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 if (process.env.NODE_ENV === 'development')
   devtools.connect('http://localhost', 8098)
@@ -19,5 +20,8 @@ app.use(createPinia())
 app.use(router)
 
 app.use(ElementPlus)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.mount('#app')
