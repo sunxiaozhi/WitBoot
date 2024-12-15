5a3201e6e50efc436e0236cbffae06f597cd6a8e
sunxiaozhi <1129535445@qq.com>
2024-12-15 23:42:27 +0800

    路由修改

--- witboot-ui/src/router/index.ts
+++ witboot-ui/src/router/index.ts
@@ -21,7 +21,7 @@ const router = createRouter({
     },
     {
       path: '/elemetDemo',
-      name: 'container',
+      name: 'elemetDemo',
       component: () => import('../views/ElementDemo.vue')
     },
   ]
