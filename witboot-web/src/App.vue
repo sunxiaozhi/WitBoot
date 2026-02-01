c9135d425cc582cb8ace89e5005e749304edd652
sunxiaozhi <1129535445@qq.com>
2026-02-01 23:46:59 +0800

    优化入口

--- witboot-web/src/App.vue
+++ witboot-web/src/App.vue
@@ -4,4 +4,9 @@
 
 <script setup lang="ts" name="App">
 import { RouterView } from 'vue-router'
+import { useMenuTabsStore } from '@/stores/menuTabsStore'
+
+// 初始化菜单标签页（必须在 pinia 注册后）
+const menuTabsStore = useMenuTabsStore()
+menuTabsStore.initHomeTab()
 </script>
